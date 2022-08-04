4272b7ad5614313771bf5df862bfa2f9d92fc73b
Diego Gobbi Slomp <diegogslomp@gmail.com>
2022-08-04 08:39:16 -0300

    Section 5: Maps

--- map/main.go
+++ map/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00FF00",
-	}
+	var colors map[string]string
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00FF00",
+	// }
 
 	fmt.Println(colors)
 }
