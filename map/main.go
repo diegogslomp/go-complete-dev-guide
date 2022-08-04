490d591349681544f0f5eec313c65733c2abbc31
Diego Gobbi Slomp <diegogslomp@gmail.com>
2022-08-04 09:03:17 -0300

    Section 5: Maps

--- map/main.go
+++ map/main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00FF00",
+		"white": "#FFFFFF",
+	}
+	printMap(colors)
+}
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00FF00",
-	// }
-
-	colors["white"] = "#FFFFFF"
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex color for", color, "is", hex)
+	}
 }
